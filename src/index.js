ce3d190eddecadf6ebe27c11d1360e2043b58bb8
Adam <adammworkman25@gmail.com>
2020-11-19 00:54:52 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelectorAll("input")[1].addEventListener("click", function (event) {
+    const lineItem = document.createElement('li');
+    const list = document.querySelector("ul#tasks");
+
+    const button = document.createElement("button");
+    button.innerHTML = "X";
+    button.onclick = () => {
+      button.parentNode.remove();
+    }
+    
+    let newTask = document.getElementById("new-task-description").value;
+    lineItem.textContent = `${newTask}  `;
+    lineItem.appendChild(button);
+    list.appendChild(lineItem);    
+    event.preventDefault();
+});
 });
